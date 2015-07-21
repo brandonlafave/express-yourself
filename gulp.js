180e067e604563da79f3148fd283c609eee2891e
Brandon LaFave <lafaveb@gmail.com>
2015-07-21 09:56:40 -0700

    Define variables for Gulp files

---
+++ gulp.js
@@ -0,0 +1,8 @@
+'use strict';
+
+var gulp = require('gulp');
+var sass = require('gulp-sass');
+var webpack = require('gulp-webpack');
+// var uglify = require('gulp-uglify');
+var minifyCss = require('gulp-minify-css');
+var minifyHTML = require('gulp-minify-html');
