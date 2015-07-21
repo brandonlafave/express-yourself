6e05dba3fadf6afa4589f4aaa7ac47a94fba1aba
Brandon LaFave <lafaveb@gmail.com>
2015-07-21 10:14:50 -0700

    Add uglify to gulp.js

--- gulp.js
+++ gulp.js
@@ -3,6 +3,6 @@
 var gulp = require('gulp');
 var sass = require('gulp-sass');
 var webpack = require('gulp-webpack');
-// var uglify = require('gulp-uglify');
+var uglify = require('gulp-uglify');
 var minifyCss = require('gulp-minify-css');
 var minifyHTML = require('gulp-minify-html');
