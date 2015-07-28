091a41b03be7f7064d0f254df913ddc2865686de
Peggy Wiltz <hengist2001@hotmail.com>
2015-07-28 15:57:27 -0700

    added a second route for new birds (post)

--- app/js/client.js
+++ app/js/client.js
@@ -13,15 +13,17 @@ require('./controllers/MainController.js')(birdWatcher);
 
 //directives
 require('./directives/navigationDirective.js')(birdWatcher);
-require('./directives/newBirdFormDirective.js')(birdWatcher);
 require('./directives/footerDirective.js')(birdWatcher);
 
 //routes
-
 birdWatcher.config(['$routeProvider', function($routeProvider) {
 	$routeProvider
 	.when('/birdlist', {
 		templateUrl: '/templates/bird-list-template.html',
+		controller: 'MainController'
+	})
+	.when('/addbird', {
+		templateUrl: '/templates/new-bird-form-template.html',
 		controller: 'MainController'
 	})
 	.otherwise({
